0566744c0efdf7160cadacfb1eceda7235b9c399
ktm06 <mikikippei@gmail.com>
2026-07-25 00:07:08 -0400

    3 state uart tx model completed

---
+++ rtl/myownuart/Serial_UART_Transmitter.sv
@@ -0,0 +1,62 @@
+module Serial_UART_Transmitter #(
+    parameter DATA_BITS = 8
+) (
+    input logic tx_send,
+    input logic [DATA_BITS-1:0] tx_data,
+    output logic tx_busy,
+    output logic uart_tx,
+    output logic tx_done,
+    input logic baud_pulse,
+    input logic CLK,
+    input logic RESET
+);
+
+logic [DATA_BITS:0]frame_bits;
+logic [3:0] frame_cnt;
+typedef enum logic [2:0] {
+    IDLE= 3'b001,
+    START=3'b010,
+    DATA=3'b100
+} State_t;
+
+
+
+State_t State;
+
+assign tx_busy = (State != IDLE);
+
+always_ff @(posedge CLK, posedge RESET) begin
+    if (RESET) begin
+        uart_tx <= 1'b1;
+        tx_done <= 1'b0;
+        State <= IDLE;
+        frame_bits <= '1;
+        frame_cnt <= '0;
+    end else begin
+        tx_done <= 1'b0;
+        if (baud_pulse) begin
+            case (State)
+                IDLE: begin
+                    tx_done <= 1'b0;
+                    if (tx_send) State <= START;
+                end
+                START: begin
+                    frame_bits <= {tx_data, 1'b0};
+                    frame_cnt <= DATA_BITS;
+                    State <= DATA;
+                end
+                DATA: begin
+                    uart_tx <= frame_bits[0];
+                    frame_bits <= {1'b1, frame_bits[DATA_BITS:1]};
+                    if (frame_cnt == 0) begin
+                        tx_done <= 1'b1;
+                        State <=IDLE;
+                    end else frame_cnt <= frame_cnt - 1'b1;
+                end
+            endcase 
+        end
+    end
+
+end
+
+endmodule
